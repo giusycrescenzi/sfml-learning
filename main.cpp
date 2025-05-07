567c34e0d2ff7ca90f75c31816f30371f0a0f504
Giuseppe Crescenzi <giuseppecrescenzi17p@gmail.com>
2025-05-07 22:58:09 +0200

    linked sfml and made a see-through useless window

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+int main() {
+    sf::Window window(sf::VideoMode({300,200}), "learning SFML",sf::Style::Default);
+    while (window.isOpen()) {
+
+    }
+    return 0;
+}
